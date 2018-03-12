c078699b3de901b98661ea96f62d5a5a6eae7cd4
EmyJH <julie.galiagrassi@protonmail.com>
2018-03-12 10:12:18 +0100

    spark streaming twitter

---
+++ Tweetstream.scala
@@ -0,0 +1,72 @@
+package twit
+
+import org.apache.spark._
+import org.apache.spark.api.java.function._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.api.java._
+import org.apache.spark.streaming.twitter._
+import twitter4j.GeoLocation
+import twitter4j.Status
+
+import twitter4j.auth._
+import twitter4j.conf._
+import org.apache.spark.streaming.{Seconds,StreamingContext}
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.twitter.TwitterUtils
+
+class Tweetstream {
+  def main(args:Array[String]) {
+        val consumerKey = "ojyylvdYrH1K02KjCN1dIAxNH";
+        val consumerSecret = "35bKepQJBpu41rwhFLAGBrVjQ9huSqdpc0kg1VAFqAD9eZxFqi";
+        val accessToken = "967042869189111808-Y5S1E2BBaQ4nQtSHcQ0D4MAZo48b6Wh";
+        val accessTokenSecret = "aOKhKKcoOqAqRSWpHy9ghgIFOFvCfXR1Xggb37X7Gljpp";
+
+        //val conf = new SparkConf()
+        //conf.setAppName("WordCount")
+        
+        val conf = new SparkConf().setMaster("local[2]").setAppName("SparkTwitterHelloWorldExample");
+        //val jssc = new StreamingContext(conf, new Duration(30000));
+        val slideInterval = new Duration(1 * 1000)
+        val jobtime = 3 * 1000
+        val ssc = new StreamingContext(conf, slideInterval)
+
+        System.setProperty("twitter4j.oauth.consumerKey", consumerKey);
+        System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret);
+        System.setProperty("twitter4j.oauth.accessToken", accessToken);
+        System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret);
+
+        val auth = Some(new OAuthAuthorization(new ConfigurationBuilder().build()))
+        val twitterStream = TwitterUtils.createStream(ssc,auth);
+
+        // Without filter: Output text of all tweets
+        val hashTagStream = twitterStream.map(_.getText).flatMap(_.split(" ")).filter(_.startsWith("#"))
+        
+        
+        
+
+        // With filter: Only use tweets with geolocation and print location+text.
+        /*JavaDStream<Status> tweetsWithLocation = twitterStream.filter(
+                new Function<Status, Boolean>() {
+                    public Boolean call(Status status){
+                        if (status.getGeoLocation() != null) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+        );
+        JavaDStream<String> statuses = tweetsWithLocation.map(
+                new Function<Status, String>() {
+                    public String call(Status status) {
+                        return status.getGeoLocation().toString() + ": " + status.getText();
+                    }
+                }
+        );*/
+
+        println(hashTagStream)
+        ssc.start();
+        
+        ssc.awaitTerminationOrTimeout(jobtime)
+    }
+}
---
+++ stream.scala
@@ -0,0 +1,89 @@
+package twit
+import org.apache.spark._
+import org.apache.spark.storage._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter.TwitterUtils
+
+
+
+import org.apache.bahir
+
+import scala.math.Ordering
+//import 
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
+
+object Wordcount {
+  def main(args: Array[String]) {
+
+  System.setProperty("twitter4j.oauth.consumerKey", "ojyylvdYrH1K02KjCN1dIAxNH")
+  System.setProperty("twitter4j.oauth.consumerSecret", "35bKepQJBpu41rwhFLAGBrVjQ9huSqdpc0kg1VAFqAD9eZxFqi")
+  System.setProperty("twitter4j.oauth.accessToken", "967042869189111808-Y5S1E2BBaQ4nQtSHcQ0D4MAZo48b6Wh")
+  System.setProperty("twitter4j.oauth.accessTokenSecret", "aOKhKKcoOqAqRSWpHy9ghgIFOFvCfXR1Xggb37X7Gljpp")
+
+  // Directory to output top hashtags
+  val outputDirectory = "/twitter"
+  
+  // Recompute the top hashtags every 1 second
+  val slideInterval = new Duration(1 * 1000)
+  
+  // Compute the top hashtags for the last 5 seconds
+  val windowLength = new Duration(5 * 1000)
+  
+  // Wait this many seconds before stopping the streaming job
+  val timeoutJobLength = 100 * 1000
+  
+  dbutils.fs.rm(outputDirectory, true)
+  
+  var newContextCreated = false
+  var num = 0
+
+  // This is a helper class used for
+  object SecondValueOrdering extends Ordering[(String, Int)] {
+    def compare(a: (String, Int), b: (String, Int)) = {
+      a._2 compare b._2
+    }
+  }
+
+  // This is the function that creates the SteamingContext and sets up the Spark Streaming job.
+ // def creatingFunc(): StreamingContext = {
+    //Create conf object
+    val conf = new SparkConf()
+    conf.setAppName("WordCount")
+   
+    //create spark context object
+    //val sc = new SparkContext(conf) 
+    // Create a Spark Streaming Context.
+    val ssc = new StreamingContext(conf, slideInterval)
+    // Create a Twitter Stream for the input source.
+    val auth = Some(new OAuthAuthorization(new ConfigurationBuilder().build()))
+    val twitterStream = TwitterUtils.createStream(ssc, auth)
+    // val twitterStream = TwitterUtils.createStream(ssc, Some(auth))
+    
+   //val cb = new ConfigurationBuildercb.setDebugEnabled(true).setOAuthConsumerKey("").setOAuthConsumerSecret("").setOAuthAccessToken("").setOAuthAccessTokenSecret("")
+   // val auth = Some(new OAuthAuthorization(cb.build))
+  
+    // Parse the tweets and gather the hashTags.
+    val hashTagStream = twitterStream.map(_.getText).flatMap(_.split(" ")).filter(_.startsWith("#"))
+  
+    // Compute the counts of each hashtag by window.
+    val windowedhashTagCountStream = hashTagStream.map((_, 1)).reduceByKeyAndWindow((x: Int, y: Int) => x + y, windowLength, slideInterval)
+  
+    // For each window, calculate the top hashtags for that time period.
+    windowedhashTagCountStream.foreachRDD(hashTagCountRDD => {
+      val topEndpoints = hashTagCountRDD.top(10)(SecondValueOrdering)
+      dbutils.fs.put(s"${outputDirectory}/top_hashtags_${num}", topEndpoints.mkString("\n"), true)
+      println(s"------ TOP HASHTAGS For window ${num}")
+      println(topEndpoints.mkString("\n"))
+      num = num + 1
+  	  })
+  
+    newContextCreated = true
+    ssc
+  }
+//}
+
+@transient val ssc = StreamingContext.getActiveOrCreate(creatingFunc)
+
+ssc.start()
+ssc.awaitTerminationOrTimeout(timeoutJobLength)
